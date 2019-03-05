373f36547e2e24019ec9c6407970a0c4517d32c2
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-03-05 11:14:32 -0700

    Fix errors so be server is up and running on port

--- src/createServer.js
+++ src/createServer.js
@@ -1,23 +1,22 @@
 const { GraphQLServer } = require('graphql-yoga');
-const Mutation = ('./resolvers/Mutation');
-const Query = ('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
 const db = require('./db');
 
-//Create GraphQL Yoga server
+// Create GraphQL Yoga Server
 
 function createServer() {
   return new GraphQLServer({
     typeDefs: 'src/schema.graphql',
     resolvers: {
       Mutation,
-      Query
+      Query,
     },
     resolverValidationOptions: {
       requireResolversForResolveType: false,
     },
-    context: req => ({ ...req, db })
+    context: req => ({ ...req, db }),
   });
-
 }
 
 module.exports = createServer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: 'variables.env' });
-const createServer = require('createServer');
+const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
@@ -8,10 +8,13 @@ const server = createServer();
 //TODO use express middleware to populate current user
 
 server.start(
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  }, deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
